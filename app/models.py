0490c894edd89a57c0da58db0c4676cbca9029e7
Ping Lee <hackercollector4@gmail.com>
2017-10-13 09:44:10 -0400

    model updated.

--- app/models.py
+++ app/models.py
@@ -1,1 +1,43 @@
 from app import db
+
+class Domain(db.Model):
+    """
+    Domains Table
+    """
+    __tablename__ = "domains"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(3), unique = True)
+
+    def __repr__(self):
+        return '<Domain: {}>'.format(self.name)
+
+class Category(db.Model):
+    """
+    Categories Table
+    """
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key = True)
+    domain_id = db.Column(db.Integer, db.ForeignKey(Domain.id), default = None)
+    parent_id = db.Column(db.Integer, default = None)
+    name = db.Column(db.String(50))
+    url = db.Column(db.UnicodeText)
+    domain = db.relationship("Domain", backref = "domain")
+
+    def __repr__(self):
+        return '<Category: {0} at {1}>'.format(self.name, self.domain.name)
+
+class Product(db.Model):
+    """
+    Products Table
+    """
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key = True)
+    domain_id = db.Column(db.Integer, db.ForeignKey(Domain.id), default = None)
+    asin = db.Column(db.String(20))
+    domain = db.relationship("Domain", backref = "domain")
+
+    def __repr__(self):
+        return '<Product: {0} at {1}>'.format(self.asin, self.domain.name)
---
+++ app/spiders/category.py
@@ -0,0 +1,19 @@
+from selenium_browser import UBrowse
+
+class CategorySpider(object):
+    """
+    Category spider for amazon products.
+    """
+    def __init__(self):
+        self.client = None
+
+    def run(self):
+        print("""
+        Starting Category Scraper....
+        """)
+        if self.client is None:
+            self.client = UBrowse()
+
+if __name__ == "__main__":
+    spider = CategorySpider()
+    spider.run()
