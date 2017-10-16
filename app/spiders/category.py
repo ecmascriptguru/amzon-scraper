0a0c6dbb2f0ba75b595d272facf8cae6e9234161
Alexis Richard <ecmascript.guru@gmail.com>
2017-10-15 23:14:02 -0400

    category spdier updated a bit.

--- app/spiders/category.py
+++ app/spiders/category.py
@@ -3,12 +3,14 @@ docsting for category class
 """
 
 from selenium_browser import UBrowse
+from ..models import *
 
 class CategorySpider(object):
     """
     Category spider for amazon products.
     """
-    def __init__(self):
+    def __init__(self, domain = "amazon.com"):
+        self.domain = Domain.query.filter_by(name=domain).first()
         self.client = None
 
     def run(self):
@@ -18,7 +20,7 @@ class CategorySpider(object):
         if self.client is None:
             self.client = UBrowse()
         
-        
+
 
 if __name__ == "__main__":
     spider = CategorySpider()
